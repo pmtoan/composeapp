1252fbf1909028a3eb8309c4018b2ee736a91809
Minh Toan <phamminhtoan304@gmail.com>
2020-06-02 23:13:36 +0700

    add app api endpoint

--- api/application_endpoint.py
+++ api/application_endpoint.py
@@ -3,6 +3,7 @@ import web
 import json
 
 from services.application.app_service import AppService
+from services.application.app_composer import AppComposer
 from services.git.git_service import GitService
 from services.docker.compose_service import ComposeService
 from services.log.build_log_service import BuildLogService
@@ -10,6 +11,7 @@ from services.log.build_log_service import BuildLogService
 
 class ApplicationEndpoint:
 	def __init__(self):
+		self.app_compose = AppComposer()
 		self.git_service = GitService(local_folder=os.environ['RESOURCES_PATH'])
 		self.compose_service = ComposeService(local_folder=os.environ['RESOURCES_PATH'])
 		self.build_log_service = BuildLogService(local_folder=os.environ['LOG_PATH'])
@@ -19,6 +21,10 @@ class ApplicationEndpoint:
 			compose_service=self.compose_service,
 			build_log_service=self.build_log_service,
 		)
+
+	def __validate_create_app_request__(self, data):
+		request = json.loads(data)
+		return self.app_compose.build_create_app_request(request['name'], request['desc'], request['repo_link'])
 
 	def GET(self, app_id):
 		web.header('Access-Control-Allow-Origin', '*')
@@ -32,3 +38,19 @@ class ApplicationEndpoint:
 				return json.dumps({})
 			else:
 				return json.dumps(repr(app))
+
+	def POST(self, app_id):
+		web.header('Access-Control-Allow-Origin', '*')
+
+		if app_id == '_':
+			# create app
+			app = self.__validate_create_app_request__(web.data())
+			result = self.app_service.create_app(app)
+			if result is None:
+				return json.dumps({'error': ''})
+			else:
+				return json.dumps({'error': result})
+		else:
+			# build app
+			return_code, output = self.app_service.build_app(app_id=app_id)
+			return json.dumps({'code': return_code, 'output': output})
