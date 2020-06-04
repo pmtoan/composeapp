d87f0ca266fe132e659be6bcfcb8bddc0882f12d
Minh Toan <phamminhtoan304@gmail.com>
2020-06-04 17:28:17 +0700

    webapp: add app detail page

--- webapp/src/detail.js
+++ webapp/src/detail.js
@@ -8,7 +8,51 @@ window.onload = function () {
 	if (CheckUserLogin()) {
 		// user already login
 		GetApp(GetAppIdFromUrl(), function (response) {
-			document.getElementById('app_detail').innerHTML = JSON.stringify(response);
+			document.getElementById('app_name').innerHTML = response['name'];
+			document.getElementById('app_parameters').innerHTML += `
+				<li class="collection-item avatar">
+					<img src="/static/images/logo.png" alt="" class="circle">
+					<span class="title" style="font-weight: bold">${response['desc']}</span>
+					<p>
+						<b>Created At: </b>${response['created_at']} <br>
+						<b>Repository: </b>${response['repository']}
+					</p>
+					<a class="secondary-content btn-flat blue-grey-text">
+						<i class="material-icons left">edit</i>
+						edit
+					</a>
+				</li>
+			`;
+			for (let i=0; i<response['build_history'].length; i++) {
+				let status = '';
+				if (response['build_history'][i]['code'] === 0) {
+					status = `
+						<a class="secondary-content green-text">
+							<i class="material-icons left">check</i>
+							success
+						</a>
+					`;
+				}
+				else {
+					status = `
+						<a class="secondary-content red-text">
+							<i class="material-icons left">clear</i>
+							failed
+						</a>
+					`;
+				}
+				document.getElementById('build_histories').innerHTML += `
+					<li class="collection-item avatar"
+					onclick="window.open('/api/log/build/${response['build_history'][i]['build_id']}', '_blank');">
+						<img src="/static/images/clock.png" alt="" class="circle">
+						<span class="title" style="font-weight: bold">${response['build_history'][i]['built_at']}</span>
+						<p>
+							Build ID:  ${response['build_history'][i]['build_id']}
+						</p>
+						${status}
+					</li>
+				`;
+			}
 		});
 
 		document.getElementById('build_app').addEventListener('click', function () {
